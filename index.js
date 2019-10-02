30286641ac8251611adcd39a3f51194dee8b97ab
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-10-02 06:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,3 +27,10 @@ function increaseRankBy(n) {
 
   }
 }
+
+/* function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}*/
